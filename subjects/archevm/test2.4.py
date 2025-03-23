180c000b04c22697af63dc20b7134a609225f875
Mikhail Voskhodin(Михаил Восходин) <starsresearchproject@gmail.com>
2025-03-24 00:53:12 +0300

    Update test2.4.py

--- subjects/archevm/test2.4.py
+++ subjects/archevm/test2.4.py
@@ -4,12 +4,12 @@ num1 = input()
 num2 = input()
 res = (bin(int(num1,2)+int(num2,2)))[2:].zfill(17)
 if res[0] == "1":
-	flag = (not flag)
-	isFrom = True
-	print("Из разряда")
-if bool(res[1]) != bool(num1[0])+bool(num2[0]):
-	flag = (not flag)
-	print("В разряд")
+  flag = (not flag)
+  isFrom = True
+  print("Из разряда")
+if res[1] != str((int(num1[0])+int(num2[0]))%2):
+  flag = (not flag)
+  print("В разряд")
 print("Переполнение" if flag==True else "Переполнения нет")
 print("Со знаком "+ ("верно" if (flag == False) else "неверно"))
 print("Без знака "+ ("верно" if (isFrom == False) else "неверно"))
